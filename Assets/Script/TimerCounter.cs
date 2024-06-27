5c3eecfde6a687df746ef0e4e6dfff4759b68b58
Yukinori-Kuwahara <chara.flarty999@gmail.com>
2024-06-27 11:44:59 +0900

    [update]バグ修正

--- Assets/Script/TimerCounter.cs
+++ Assets/Script/TimerCounter.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class TimerCounter : MonoBehaviour
 {
+    Camera _cam;
     AudioSource m_AudioSource;
     GameObject m_timeText = default;
     float _nowtime;
@@ -22,6 +23,7 @@ public class TimerCounter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _cam = Camera.main;
         _nowtime = 0;
         m_AudioSource = GetComponent<AudioSource>();
         player = GameObject.Find("Player");
@@ -31,25 +33,27 @@ public class TimerCounter : MonoBehaviour
         m_BGM = m_bgm.GetComponent<AudioSource>();
         m_spawn = GameObject.Find("SpawnArea");
         m_timeText = GameObject.Find("TimeText");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = _cam.transform.position;
         _nowtime += Time.deltaTime;
         _time = _maxtime - _nowtime;
         m_timeText.GetComponent<Text>().text = _time.ToString("F2");
         if (_time < 0)
         {
             Destroy(player); Destroy(m_spawn);
-            m_AudioSource.PlayOneShot(m_Clear);
+            AudioSource.PlayClipAtPoint(m_Clear,pos);
             Text gameoverText = m_gameover.GetComponent<Text>();
             gameoverText.text = "Congratulations!";
             ButtonUIObj.SetActive(true);
             Text lifeText = m_lifeText.GetComponent<Text>();
             lifeText.text = " ";
-            Destroy(gameObject, 2f);
             m_BGM.mute = true;
+            Destroy(gameObject);
         }
     }
 }
